b04465dc3f4aebe4e7f2920e7fb3b0a4a20b756a
Mika Herrmann <mika.herrmann@js-soft.com>
2026-07-21 14:49:52 +0200

    Add saving functionality for the audio decoder

--- src/formats/audio.cpp
+++ src/formats/audio.cpp
@@ -57,84 +57,125 @@ namespace LR1 {
 
     std::optional<PcmData> AudioDecoder::decode() {
         //Find streams
-    if (avformat_find_stream_info(ctx, nullptr) < 0) {
-        cerr << "Couldn't find stream information" << endl;
-        return nullopt;
-    }
-
-    int streamIndex = -1;
-    for (int i = 0; i < ctx->nb_streams; i++) {
-        if (ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            streamIndex = i;
-            break;
+        if (avformat_find_stream_info(ctx, nullptr) < 0) {
+            cerr << "Couldn't find stream information" << endl;
+            return nullopt;
         }
-    }
 
-    if (streamIndex == -1) {
-        cerr << "No audio stream found" << endl;
-        return nullopt;
-    }
-
-    // Get Codec parameters and codec context
-    const AVCodecParameters* codecParams = ctx->streams[streamIndex]->codecpar;
-    const AVCodec* codec = avcodec_find_decoder(codecParams->codec_id);
-    AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
-    avcodec_parameters_to_context(codecCtx, codecParams);
-
-    //Open Codec
-    if (avcodec_open2(codecCtx, codec, nullptr) < 0) {
-        cerr << "Couldn't open codec" << endl;
-        return nullopt;
-    }
-
-    cout << "Opened codec " << codec->long_name << endl;
-    cout << "Sample Rate: " << codecCtx->sample_rate << endl;
-    cout << "Channels: " << codecCtx->ch_layout.nb_channels << endl;
-    cout << "Bits: " << (av_get_bytes_per_sample(codecCtx->sample_fmt) * 8) << endl;
-
-    const int sampleRate = codecCtx->sample_rate;
-    const int channels = codecCtx->ch_layout.nb_channels;
-    vector<int16_t> data;
-
-    //Alloc frame and packet
-    AVFrame* frame = av_frame_alloc();
-    AVPacket* packet = av_packet_alloc();
-
-    //Read frames from the audio stream
-    while (av_read_frame(ctx, packet) >= 0) {
-        if (packet->stream_index == streamIndex) {
-            int ret = avcodec_send_packet(codecCtx, packet);
-            if (ret < 0) {
-                cerr << "Couldn't send packet for decoding" << endl;
+        int streamIndex = -1;
+        for (int i = 0; i < ctx->nb_streams; i++) {
+            if (ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+                streamIndex = i;
                 break;
             }
-
-            while (true) {
-                ret = avcodec_receive_frame(codecCtx, frame);
-                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-                    break;
-                }
-                if (ret < 0) {
-                    cerr << "Error during decoding" << endl;
-                    break;
-                }
-
-                const size_t currentBufSize = data.size();
-                const int bytesPerSample = av_get_bytes_per_sample(codecCtx->sample_fmt);
-                const size_t totalSamples = frame->nb_samples * channels;
-                data.resize(currentBufSize + totalSamples);
-                memcpy(data.data() + currentBufSize, frame->data[0], totalSamples * bytesPerSample);
-            }
         }
 
-        av_packet_unref(packet);
+        if (streamIndex == -1) {
+            cerr << "No audio stream found" << endl;
+            return nullopt;
+        }
+
+        // Get Codec parameters and codec context
+        const AVCodecParameters* codecParams = ctx->streams[streamIndex]->codecpar;
+        const AVCodec* codec = avcodec_find_decoder(codecParams->codec_id);
+        AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
+        avcodec_parameters_to_context(codecCtx, codecParams);
+
+        //Open Codec
+        if (avcodec_open2(codecCtx, codec, nullptr) < 0) {
+            cerr << "Couldn't open codec" << endl;
+            return nullopt;
+        }
+
+        cout << "Opened codec " << codec->long_name << endl;
+        cout << "Sample Rate: " << codecCtx->sample_rate << endl;
+        cout << "Channels: " << codecCtx->ch_layout.nb_channels << endl;
+        cout << "Bits: " << (av_get_bytes_per_sample(codecCtx->sample_fmt) * 8) << endl;
+
+        const int sampleRate = codecCtx->sample_rate;
+        const int channels = codecCtx->ch_layout.nb_channels;
+        vector<int16_t> data;
+
+        //Alloc frame and packet
+        AVFrame* frame = av_frame_alloc();
+        AVPacket* packet = av_packet_alloc();
+
+        //Read frames from the audio stream
+        while (av_read_frame(ctx, packet) >= 0) {
+            if (packet->stream_index == streamIndex) {
+                int ret = avcodec_send_packet(codecCtx, packet);
+                if (ret < 0) {
+                    cerr << "Couldn't send packet for decoding" << endl;
+                    break;
+                }
+
+                while (true) {
+                    ret = avcodec_receive_frame(codecCtx, frame);
+                    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                        break;
+                    }
+                    if (ret < 0) {
+                        cerr << "Error during decoding" << endl;
+                        break;
+                    }
+
+                    const size_t currentBufSize = data.size();
+                    const int bytesPerSample = av_get_bytes_per_sample(codecCtx->sample_fmt);
+                    const size_t totalSamples = frame->nb_samples * channels;
+                    data.resize(currentBufSize + totalSamples);
+                    memcpy(data.data() + currentBufSize, frame->data[0], totalSamples * bytesPerSample);
+                }
+            }
+
+            av_packet_unref(packet);
+        }
+
+        av_frame_free(&frame);
+        av_packet_free(&packet);
+        avcodec_free_context(&codecCtx);
+
+        return {{sampleRate, channels, data}};
     }
 
-    av_frame_free(&frame);
-    av_packet_free(&packet);
-    avcodec_free_context(&codecCtx);
+    bool AudioDecoder::save(const path& path, const PcmData& decoded) {
+        ofstream file(path, ios::binary);
+        if (!file) return false;
 
-    return {{sampleRate, channels, data}};
+        //Header
+        constexpr uint32_t headerSize = 44;
+        const uint32_t dataSize = decoded.data.size() * sizeof(int16_t);
+        const uint32_t totalSize = headerSize + dataSize - 8;
+
+        file << "RIFF";
+        file.write(reinterpret_cast<const char*>(&totalSize), 4);
+        file << "WAVE";
+
+        //Chunk
+        constexpr uint32_t chunkSize = 16;
+        constexpr uint16_t audioFormat = 1;
+        const uint16_t numChannels = decoded.channels;
+        const uint32_t sampleRate = decoded.sampleRate;
+        constexpr uint16_t bitsPerSample = 16;
+        const uint16_t bytesPerBlock = numChannels * bitsPerSample / 8;
+        const uint32_t bytesPerSecond = sampleRate;
+
+        file << "fmt ";
+        file.write(reinterpret_cast<const char*>(&chunkSize), 4);
+        file.write(reinterpret_cast<const char*>(&audioFormat), 2);
+        file.write(reinterpret_cast<const char*>(&numChannels), 2);
+        file.write(reinterpret_cast<const char*>(&sampleRate), 4);
+        file.write(reinterpret_cast<const char*>(&bytesPerSecond), 4);
+        file.write(reinterpret_cast<const char*>(&bytesPerBlock), 2);
+        file.write(reinterpret_cast<const char*>(&bitsPerSample), 2);
+
+        //Data
+        file << "data";
+        file.write(reinterpret_cast<const char*>(&dataSize), 4);
+        file.write(reinterpret_cast<const char*>(decoded.data.data()), dataSize);
+
+        file.close();
+
+        return true;
     }
 
     MemoryIO::MemoryIO(const std::vector<uint8_t> &data) : ctx(nullptr), buf(nullptr), data(data), offset(0) {
